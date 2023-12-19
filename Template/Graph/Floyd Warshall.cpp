866e1b23e4003833bcb5226cd373f278cb38d837
allex0604 <47781627+allex0604@users.noreply.github.com>
2023-12-19 08:16:39 +0000

    add floyd warshall

---
+++ Template/Graph/Floyd Warshall.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define PLI pair<ll, int>
+
+const int N = 3e5 + 9, mod = 1e9 + 7, INF = 1e9;
+
+int32_t main()
+{
+    int n , m; cin >> n >> m;
+    vector<vector<int>> d(n+1, vector<int>(n+1, INF));
+    for(int i=0; i<m; i++){
+        int u, v; cin >> u >> v;
+        cin >> d[u][v];
+    }
+    for(int k = 1; k <= n; k++){
+        for(int src=1; src <= n; src++){
+            for(int dst=1; dst <=n ;dst++){
+                d[src][dst] = min(d[src][dst], d[src][k] + d[k][dst]);
+            }
+        }
+    }
+    return 0;
+}
